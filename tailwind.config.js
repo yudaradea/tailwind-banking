b1efebbeb6b283d8ae3c907d7582fc60cb02e6bf
Yuda <yudaaradeagit@gmail.com>
2024-05-04 07:33:30 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin");
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".-rotate-y-180": {
+      transform: "rotateY(-180deg)",
+    },
+  });
+});
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "color-primary": "#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-dark": "#010417",
+        "color-secondary": "#ff7d3b",
+        "color-gray": "#333",
+        "color-white": "#fff",
+        "color-blog": "#A427DF",
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: "20px",
+        md: "50px",
+        // sm: "2rem",
+        // lg: "4rem",
+        // xl: "5rem",
+        // "2xl": "6rem",
+      },
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1496px",
+      },
+      center: true,
+    },
+  },
+  plugins: [rotateY],
+};
